62a7c52ad5558a171c09820c5d5fdd5d87bc8c70
JamirLeal <a00826275@itesm.mx>
2021-05-25 20:54:47 -0600

    Created a publisher for path planning (path_planning_output).

--- vsss_ws/src/path_planning/path_planning/path_planning.py
+++ vsss_ws/src/path_planning/path_planning/path_planning.py
@@ -1,17 +1,23 @@
 import rclpy
 from rclpy.node import Node
-from vsss_interfaces.msg import StrategyTopic
-from geometry_msgs.msg import Pose, Point, Twist
+from vsss_interfaces.msg import StrategyTopic, PathPlanningTopic
+from geometry_msgs.msg import Pose, PoseArray
 
 class PathPlanning(Node):
     def __init__(self):
         super().__init__('path_planning')
         self.suscriber_ = self.create_subscription(StrategyTopic, 'strategy_output', self.callback_path_planning, 10)
+        self.publisher_ = self.create_publisher(PathPlanningTopic, 'path_planning_output', 10)
 
     def callback_path_planning(self, msg):
         for robot in msg.robot_positions:
             self.get_logger().info("Receiving robot information")
         self.get_logger().info("Robots info received")
+
+        newMsg = PathPlanningTopic()
+        [newMsg.trajectories.append(PoseArray()) for _ in range(3)]
+        self.publisher_.publish(newMsg)
+        self.get_logger().info("Robots path sento to control.")
 
 def main(args=None):
     rclpy.init(args=args)
